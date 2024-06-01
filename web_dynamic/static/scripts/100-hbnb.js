f52592159ec4ee3abe4cc24c395dc87aa5935801
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-06-01 12:37:32 -0700

    Task 5: States and Cities

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  const locations = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      locations[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete locations[$(this).data('id')];
+    }
+    $('.locations h4').text(Object.values(locations).join(', '));
+  });
+
+  $('button').click(function () {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ locations: Object.keys(locations) }),
+      success: function (data) {
+        $('section.places').empty();
+        for (let place of data) {
+          $('section.places').append(
+            `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guests</div>
+                <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`
+          );
+        }
+      }
+    });
+  });
+});
